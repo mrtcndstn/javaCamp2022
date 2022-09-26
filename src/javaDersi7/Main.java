fc86f254302c1a614c4703b003ad80154f2e620f
mertcan dastan <mertcandastan@gmail.com>
2022-09-26 18:51:04 +0300

    update

--- src/javaDersi7/Main.java
+++ src/javaDersi7/Main.java
@@ -5,6 +5,36 @@ public class Main {
 	public static void main(String[] args) {
 		
 
+		int sayi =12;
+		sayi = 11;
+		
+		byte sayi2 =12;
+		sayi2 = 120;
+		
+		double sayi3 =12.5;
+		sayi3 = 12.9;
+		
+		char karakter = 'a';
+		
+		boolean dogruMu=false;
+        		
+		
+		
+		
+		
+			/**
+		 * int tipinde bir değişkene String bir değer atandığında hata verir.
+ Primitive type(İlkel Tip)
+ - Tam Sayı Tipleri: byte, short, int, long
+ * int tam sayıları tutar. 1,6,44,111,-111 gibi - 2 milyar ile - 2 milyar arasında bir değer alabilir.
+ * byte - 128 ile 127 arasında bir değer alabilir.
+ * short - 32 bin ile 32 bin arasında bir değer alabilir.
+ * long - 9 milyar ile 9 milyar arasında bir değer alabilir.
+ 
+    - Ondalıklı Sayı Tipleri: float, double
+    - char: karakterlerin tutulduğu tip
+    - boolean: true/false değerlerini tutar.
+		 */
 	}
 
 }
