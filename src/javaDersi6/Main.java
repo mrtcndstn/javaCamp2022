9283417bef8c49cd031e7ceb2656be44c7a65fe7
mertcan dastan <mertcandastan@gmail.com>
2022-09-26 17:51:41 +0300

    first commit

---
+++ src/javaDersi6/Main.java
@@ -0,0 +1,27 @@
+package javaDersi6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi = 10;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj  + ogrenciSayisi);
+		
+		
+		/**
+		*System.out.println("Ogrenci sayım : 9");
+		*System.out.println("Ogrenci sayım : 9");
+		*System.out.println("Ogrenci sayım : 9");
+		*System.out.println("Ogrenci sayım : 9");
+		*/
+		
+		/**
+		 * Java büyük, küçük harf duyarlıdır.
+		 * Değişkenleri kod tekrarı olmaması için kullanırız.
+		 * İsimlendirme de 'camelCase' tarzı tercih edilir. (Örnek : ögrenciSayısı)
+		 * String metinsel veriler için kullanılırız. ("Öğrenci sayısı : "); şeklinde yazılır
+		 */
+	}
+
+}
