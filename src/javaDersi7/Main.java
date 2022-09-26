d145d014dd6c87614b25dc3b6307ab99f007e755
mertcan dastan <mertcandastan@gmail.com>
2022-09-26 19:13:12 +0300

    update

--- src/javaDersi7/Main.java
+++ src/javaDersi7/Main.java
@@ -22,19 +22,19 @@ public class Main {
 		
 		
 		
-			/**
-		 * int tipinde bir değişkene String bir değer atandığında hata verir.
- Primitive type(İlkel Tip)
- - Tam Sayı Tipleri: byte, short, int, long
- * int tam sayıları tutar. 1,6,44,111,-111 gibi - 2 milyar ile - 2 milyar arasında bir değer alabilir.
- * byte - 128 ile 127 arasında bir değer alabilir.
- * short - 32 bin ile 32 bin arasında bir değer alabilir.
- * long - 9 milyar ile 9 milyar arasında bir değer alabilir.
+	/**
+	* int tipinde bir değişkene String bir değer atandığında hata verir.
+      Primitive type(İlkel Tip)
+     - Tam Sayı Tipleri: byte, short, int, long
+    * int tam sayıları tutar. 1,6,44,111,-111 gibi - 2 milyar ile - 2 milyar arasında bir değer alabilir.
+    * byte - 128 ile 127 arasında bir değer alabilir.
+    * short - 32 bin ile 32 bin arasında bir değer alabilir.
+    * long - 9 milyar ile 9 milyar arasında bir değer alabilir.
  
     - Ondalıklı Sayı Tipleri: float, double
     - char: karakterlerin tutulduğu tip
     - boolean: true/false değerlerini tutar.
-		 */
+	*/
 	}
 
 }
