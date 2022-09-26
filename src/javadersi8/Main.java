b03030495c6e4aac77ffccbb08514180c8e48191
mertcan dastan <mertcandastan@gmail.com>
2022-09-26 18:57:40 +0300

    update

---
+++ src/javadersi8/Main.java
@@ -0,0 +1,33 @@
+package javadersi8;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/**
+		 if(kosul) {
+		      // kosul sağlandığında yapılacak işlemler
+		 }elseif(kosul1) {
+		      // kosul1 sağlandığında yapılacak işlemler
+		 }else {
+		      // kosullar sağlanmadığında yapılacak işlemler
+		    }
+		 */
+
+	}
+
+}
