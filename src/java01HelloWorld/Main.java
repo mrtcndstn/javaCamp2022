9b3ac2c21e57c532ee3953f69ae16fa92d872922
mertcan dastan <mertcandastan@gmail.com>
2022-09-27 13:02:46 +0300

    update

---
+++ src/java01HelloWorld/Main.java
@@ -0,0 +1,16 @@
+package java01HelloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba Dunya");
+		System.out.println("Merhaba Dunya 2");
+		
+		/**
+		 * Bu işlem ekrana yazdırma işlemi yapar.
+		 * Her satır sonunda ';' kullanılmalıdır.
+		 */
+
+	}
+
+}
